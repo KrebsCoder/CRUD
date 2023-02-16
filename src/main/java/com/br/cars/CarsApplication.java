6a5986ec3afcbf5f9c9c9f01772a35613e8c2cfb
KrebsCoder <lucasklopess@gmail.com>
2023-02-16 15:23:38 -0300

    feat: changed @SpringBootApplication annotation

--- src/main/java/com/br/cars/CarsApplication.java
+++ src/main/java/com/br/cars/CarsApplication.java
@@ -1,9 +1,14 @@
 package com.br.cars;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@ComponentScan(basePackages = {"com.br.cars.services"})
 public class CarsApplication {
 
     public static void main(String[] args) {
