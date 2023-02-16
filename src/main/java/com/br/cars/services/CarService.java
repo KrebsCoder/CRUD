68c1a74dbd054e28fb8cb75c14db5460746e247a
KrebsCoder <lucasklopess@gmail.com>
2023-02-16 15:28:14 -0300

    feat: services is implemented.

---
+++ src/main/java/com/br/cars/services/CarService.java
@@ -0,0 +1,46 @@
+package com.br.cars.services;
+
+import com.br.cars.CarsApplication;
+import com.br.cars.models.CarModel;
+import com.br.cars.repositories.CarRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarService {
+    final CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @Transactional
+    public CarModel addCar(CarModel car){
+         Optional<CarModel> optionalCar = carRepository.findByNome(car.getNome());
+         if (optionalCar.isPresent()){
+             throw new RuntimeException("Car already exists in the database.");
+         }
+        return carRepository.save(new CarModel(car.getNome(), car.getAno(), car.getValorTabelaFipe()));
+    }
+
+    public CarModel findCarByName(String carName){
+        Optional<CarModel> car = carRepository.findByNome(carName);
+        if (car.isEmpty()){
+            throw new RuntimeException("Car does not exist in the database.");
+        }
+        return car.get();
+    }
+
+    public List<CarModel> findAllCars() {
+        Optional<List<CarModel>> cars = Optional.of(carRepository.findAll());
+        if (!cars.isPresent()){
+            throw new RuntimeException("There are no cars in the database.");
+        }
+        return cars.get();
+    }
+}
